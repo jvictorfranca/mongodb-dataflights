f928c9e3c121ba34665241da10c7106ff77df73d
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-11-17 15:03:36 -0300

    faz o 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,8 @@
+use("dataFlights");
+db.voos.findOne({ $and: [
+  {
+    $or: [
+      { "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" },
+    ],
+  }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" },
+] }, { vooId: true, _id: false });
