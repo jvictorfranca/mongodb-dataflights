6a90a8aef23daa497121cbeb250cbf67804fbc69
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-11-17 13:02:12 -0300

    faz o 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,1 @@
+db.voos.find({ ano: 2017, "empresa.nome": "GOL" }, { empresa: { nome: true }, vooId: true, aeroportoOrigem: { nome: true }, aeroportoDestino: { nome: true }, mes: true, ano: true, _id: false }).limit(10);
