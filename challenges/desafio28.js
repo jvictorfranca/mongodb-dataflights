7ea9bdeda44d2e9a3dbda2c30d7d457698779205
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-11-17 15:56:31 -0300

    corrige ja o 28 sem ver o resultado msm

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,6 +1,7 @@
-const count = db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" });
+const text = "LATAM AIRLINES BRASIL";
+const count = db.voos.count({ "empresa.nome": text, natureza: "Doméstica" });
 
-db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: count });
+db.resumoVoos.insertOne({ empresa: text, totalVoosDomesticos: count });
 db.resumoVoos.findOne(
-  { empresa: "LATAM AIRLINES BRASIL" }, { _id: false, empresa: true, totalVoosDomesticos: true },
+  { empresa: text }, { _id: false, empresa: true, totalVoosDomesticos: true },
   );
